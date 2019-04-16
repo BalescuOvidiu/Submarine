d66ea42631cef9dfe6656d684e542ea8ff6296eb
Balescu Ovidiu Gheorghe <balescuovidiu@gmail.com>
2019-04-17 00:27:22 +0300

    Add files via upload

--- src/submarin.ino
+++ src/submarin.ino
@@ -3,6 +3,24 @@
 #include <RobotIRremoteInt.h>
 #include <RobotIRremoteTools.h>
 
+// Time
+#define SECOND 1000
+#define MINUTE 60000
+#define HOUR   3600000
+#define DAY    86400000
+// Color
+#define BLUE     0,   0, 255
+#define GREEN    0, 255,   0
+#define RED    255,   0,   0
+#define YELLOW 255, 255,   0
+#define WHITE  255, 255, 255
+// Angle
+#define ANGLE_MIN       0
+#define ANGLE_MID      90
+#define ANGLE_MAX     180
+// Numbers
+#define MIN             0
+#define MAX           255
 // RGB LED
 #define PIN_RED         2
 #define PIN_GREEN       3
@@ -18,184 +36,302 @@
 // Receiver
 #define PIN_RECEIVER    10
 
+// Time interval
+class TimeInterval {
+  private:
+    unsigned long start;
+    unsigned long duration;
+  public:
+    TimeInterval(unsigned long start, unsigned long duration);
+    bool isElapsed();
+    bool elapse();
+    void restart();
+    void restart(unsigned long duration);
+    ~TimeInterval();
+};
+TimeInterval::TimeInterval(unsigned long start, unsigned long duration) {
+  this->start = start;
+  this->duration = duration;
+}
+bool TimeInterval::isElapsed() {
+  return this->start + this->duration <= millis();
+}
+bool TimeInterval::elapse() {
+  if (this->isElapsed()) {
+    this->restart();
+    return 1;
+  }
+  return 0;
+}
+void TimeInterval::restart() {
+  this->start = millis();
+}
+void TimeInterval::restart(unsigned long duration) {
+  this->start = millis();
+  this->duration = duration;
+}
+TimeInterval::~TimeInterval() {
+}
+
+// RGB
+class Color {
+  public:
+    short red;
+    short green;
+    short blue;
+    Color();
+    Color(short red, short green, short blue);
+    ~Color();
+};
+Color::Color() {
+  this->red = 0;
+  this->green = 0;
+  this->blue = 0;
+}
+Color::Color(short red, short green, short blue) {
+  this->red = red;
+  this->green = green;
+  this->blue = blue;
+}
+Color::~Color() {
+}
+
 // RGB LED
-unsigned ledRed = 0;
-unsigned ledGreen = 0;
-unsigned ledBlue = 0;
-// Servomotors
-Servo sternLeft;
-Servo sternRight;
-Servo rudder;
-int sternLeftAngle = 0;
-int sternRightAngle = 0;
-int rudderAngle = 0;
-int sternLeftDir = 1;
-int sternRightDir = 1;
-int rudderDir = 1;
-int sternLeftTest=0;
-int sternRightTest=0;
-int rudderTest=0;
-// Propeller
-int propellerA = LOW;
-int propellerB = LOW;
+class Led {
+  private:
+    Color color;
+    Color pin;
+    bool commonAnode;
+  public:
+    Led(Color color, bool commonAnode);
+    Led(short red, short green, short blue, bool commonAnode);
+    void loop();
+    void write(short red, short green, short blue);
+    void write(Color color);
+    void error();
+    ~Led();
+};
+Led::Led(Color color, bool commonAnode) {
+  pinMode(color.red, OUTPUT);
+  pinMode(color.green, OUTPUT);
+  pinMode(color.blue, OUTPUT);
+  this->pin = color;
+  this->color = Color();
+  this->commonAnode = commonAnode;
+}
+Led::Led(short red, short green, short blue, bool commonAnode) {
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+  this->pin = Color(red, green, blue);
+  this->color = Color();
+  this->commonAnode = commonAnode;
+}
+void Led::loop() {
+  if (this->commonAnode) {
+    analogWrite(this->pin.red, this->color.red);
+    analogWrite(this->pin.green, this->color.green);
+    analogWrite(this->pin.blue, this->color.blue);
+  }
+  else {
+    analogWrite(this->pin.red, MAX - this->color.red);
+    analogWrite(this->pin.green, MAX - this->color.green);
+    analogWrite(this->pin.blue, MAX - this->color.blue);
+  }
+}
+void Led::write(short red, short green, short blue) {
+  this->color = Color(red, green, blue);
+}
+void Led::write(Color color) {
+  this->color = color;
+}
+void Led::error() {
+  this->write(RED);
+}
+Led::~Led() {
+}
+
+// Servomotor
+class Servomotor {
+  private:
+    Servo s;
+    short angle;
+    short dir;
+    short pin;
+    bool test;
+  public:
+    Servomotor(short pin, short angle);
+    void write(short angle);
+    void loop();
+    void attach(short pin);
+    void detach();
+    void reset();
+    void enableTest();
+    void disableTest();
+    void testIt();
+    ~Servomotor();
+};
+Servomotor::Servomotor(short pin, short angle) {
+  this->pin = pin;
+  this->attach(pin);
+  this->write(angle);
+}
+void Servomotor::loop() {
+  this->s.write(this->angle);
+}
+void Servomotor::write(short angle) {
+  this->angle = angle;
+  this->loop();
+}
+void Servomotor::attach(short pin) {
+  this->s.attach(pin);
+  this->reset();
+}
+void Servomotor::detach() {
+  this->s.detach();
+}
+void Servomotor::reset() {
+  this->dir = 1;
+  this->test = false;
+}
+void Servomotor::enableTest() {
+  this->test = 1;
+}
+void Servomotor::disableTest() {
+  this->test = 0;
+}
+void Servomotor::testIt() {
+  if (this->test) {
+    if (angle == ANGLE_MIN || angle == ANGLE_MAX) {
+      dir *= -1;
+    }
+    this->angle += dir;
+    this->loop();
+  }
+}
+Servomotor::~Servomotor() {
+  this->detach();
+}
+
+// Motor
+class Motor {
+  private:
+    short a;
+    short b;
+    short pinA;
+    short pinB;
+  public:
+    Motor(short a, short b, short pinA, short pinB);
+    void loop();
+    void write(short a, short b);
+    void stop();
+    void forward(short speed);
+    void backward(short speed);
+    ~Motor();
+};
+Motor::Motor(short a, short b, short pinA, short pinB) {
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  this->a = a;
+  this->b = b;
+  this->pinA = pinA;
+  this->pinB = pinB;
+}
+void Motor::loop() {
+  analogWrite(pinA, a);
+  analogWrite(pinB, b);
+}
+void Motor::write(short a, short b) {
+  this->a = a;
+  this->b = b;
+  loop();
+}
+void Motor::stop() {
+  this->write(0, 0);
+}
+void Motor::forward(short speed) {
+  this->write(speed, 0);
+}
+void Motor::backward(short speed) {
+  this->write(0, speed);
+}
+Motor::~Motor() {
+}
+
+// Variable
+bool shore = true;
+unsigned long idServo = 0;
+
+// Time interval
+TimeInterval timeCommand(0, 0);
+TimeInterval timeServo(0, 800);
+
+// Objects
+Led output(PIN_RED, PIN_GREEN, PIN_BLUE, false);
+Motor propeller(0, 0, PIN_PROPELLER_A, PIN_PROPELLER_B);
+Servomotor sternLeft(PIN_STERN_LEFT, ANGLE_MID);
+Servomotor sternRight(PIN_STERN_RIGHT, ANGLE_MID);
+Servomotor rudder(PIN_RUDDER, ANGLE_MID);
+
 // Receiver
 IRrecv receiver(PIN_RECEIVER);
 decode_results receiverResult;
-// Commands
-unsigned long duration = 0;
-unsigned long start = 0;
-unsigned long route = 0;
-int shore = 1;
 
-// RGB LED
-void setColor(unsigned red, unsigned green, unsigned blue) {
-  ledRed = 255 - red;
-  ledGreen = 255 - green;
-  ledBlue = 255 - blue;
-}
-void printLed() {
-  analogWrite(PIN_RED, ledRed);
-  analogWrite(PIN_GREEN, ledGreen);
-  analogWrite(PIN_BLUE, ledBlue);
-}
-void error() {
-  setColor(255, 0, 0);
-}
-void up() {
-  setColor(0, 0, 255);
-}
-void down() {
-  setColor(255, 64, 0);
-}
-void getColor() {
-  if (sternLeftAngle < 0 && sternRightAngle < 0) {
-    down();
+// Other functions
+void loopServo() {
+  if (timeServo.elapse()) {
+    switch (idServo) {
+      case 0: sternLeft.loop(); break;
+      case 1: sternRight.loop(); break;
+      case 2: rudder.loop(); break;
+      default: break;
+    }
+    idServo = (idServo + 1) % 3;
   }
-  else if (sternLeftAngle > 0 && sternRightAngle > 0) {
-    up();
-  }
-}
-// Servomotors
-void setRudder(int angle) {
-  rudder.write(MID + angle);
-  rudderAngle = angle;
-}
-void setSternLeft(int angle) {
-  sternLeft.write(MID + angle);
-  sternLeftAngle = angle;
-}
-void setSternRight(int angle) {
-  sternRight.write(MID + angle);
-  sternRightAngle = angle;
-}
-void setSterns(int angle) {
-  setSternLeft(angle);
-  setSternRight(angle);
-  getColor();
-}
-// Propeller
-void propeller() {
-  if (propellerA || propellerB) {
-    digitalWrite(PIN_PROPELLER_A, propellerA);
-    digitalWrite(PIN_PROPELLER_B, propellerB);
-  }
-}
-void forward() {
-  propellerA = HIGH;
-  propellerB = LOW;
-  setColor(0, 0, 255);
 }
 void wait() {
-  propellerA = 0;
-  propellerB = 0;
-  digitalWrite(PIN_PROPELLER_A, propellerA);
-  digitalWrite(PIN_PROPELLER_B, propellerB);
-  setColor(255, 255, 255);
-  setSterns(30);
-  setRudder(0);
+  shore = true;
+  propeller.stop();
+  output.write(WHITE);
+  rudder.write(0);
 }
-// Commands
-void go(unsigned long seconds) {
-  setSterns(0);
-  duration = seconds * 1000;
-  shore = 0;
-  forward();
-  start = millis();
+void go(unsigned long duration, short speed, int dir) {
+  output.write(BLUE);
+  shore = false;
+  rudder.write(ANGLE_MID + dir);
+  propeller.forward(speed);
+  timeCommand.restart(duration);
 }
-void testLeftStern() {
-  if(sternLeftTest){
-    setSternLeft((sternLeftAngle + sternLeftDir));
-    if (sternLeftAngle == 0 || sternLeftAngle == 90) {
-      sternLeftDir *= -1;
-    }
-    delay(20);
-  }
-}
-void testRightStern() {
-  if(sternRightTest){
-    setSternRight((sternRightAngle + sternRightDir));
-    if (sternRightAngle == 0 || sternRightAngle == 90) {
-      sternRightDir *= -1;
-    }
-    delay(20);
-  }
-}
-void testSterns() {
-  testLeftStern();
-  testRightStern();
-  delay(20);
-}
-void testRudder() {
-  if(rudderTest){
-    setRudder((rudderAngle + rudderDir));
-    if (rudderAngle == 0 || rudderAngle == 90) {
-      rudderDir *= -1;
-    }
-    delay(20);
-  }
-}
-// Receiver
 void receiveCommand() {
   if (shore) {
     if (receiver.decode(&receiverResult)) {
       Serial.println(receiverResult.value, HEX);
-      go(15);
+      go(5 * SECOND, 255, 0);
       receiver.resume();
     }
-
   }
   else {
-    if (start + duration <= millis()) {
+    if (timeCommand.isElapsed()) {
       wait();
-      shore = 1;
     }
   }
 }
+
 // Main functions
 void setup() {
   Serial.begin(9600);
-  // RGB LED
-  pinMode(PIN_RED, OUTPUT);
-  pinMode(PIN_GREEN, OUTPUT);
-  pinMode(PIN_BLUE, OUTPUT);
-  // Servomotors
-  sternLeft.attach(PIN_STERN_LEFT);
-  sternRight.attach(PIN_STERN_RIGHT);
-  rudder.attach(PIN_RUDDER);
-  // Propeller
-  pinMode(PIN_PROPELLER_A, OUTPUT);
-  pinMode(PIN_PROPELLER_B, OUTPUT);
   wait();
   // Receiver
   receiver.enableIRIn();
 }
-int angle = 0;
-int dir = 1;
 void loop() {
-  testLeftStern();
-  testRightStern();
-  testRudder();
-  printLed();
-  propeller();
+  propeller.loop();
+  // Communication
+  output.loop();
   receiveCommand();
+  // Servomotors
+  loopServo();
+  sternLeft.testIt();
+  sternRight.testIt();
+  rudder.testIt();
 }
